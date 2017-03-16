6cfa1fdbcbc5a1063ef31d7acb76796202dd6a1c
garazi <grewis@grewis-ltm3.internal.salesforce.com>
2017-03-16 08:45:29 -0700

    migrated from an older repo

---
+++ Exercise_5/src/aura/SimilarProperties/SimilarPropertiesController.js
@@ -0,0 +1,8 @@
+({
+    doInit : function(component, event, helper) {
+        helper.getProps(component);
+    },
+    doSearch : function (component, event, helper) {
+        helper.getProps(component);
+    }
+})
