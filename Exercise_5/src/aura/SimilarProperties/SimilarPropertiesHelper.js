28e699da85188f3d1013368ff843ddf77145c0a7
Christophe Coenraets <ccoenraets@ccoenraets-ltm1.internal.salesforce.com>
2017-03-19 14:28:10 -0400

    adding solution files

--- Exercise_5/src/aura/SimilarProperties/SimilarPropertiesHelper.js
+++ Exercise_5/src/aura/SimilarProperties/SimilarPropertiesHelper.js
@@ -1,16 +1,5 @@
 ({
-    getProps : function(component) {
-        var recId = component.get("v.recordId");
-        var compValue = component.get("v.searchCriteria");
-        var action = component.get("c.getSimilarProperties");
-        action.setParams({
-            recordId: recId,
-            searchCriteria: compValue
-        });
-        action.setCallback(this, function(response){
-            var res = response.getReturnValue();
-            component.set("v.similarProperties", res);
-        });
-        $A.enqueueAction(action);
+    helperMethod : function() {
+
     }
 })
